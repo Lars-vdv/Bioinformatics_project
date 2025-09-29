a7b66071373e884a20737f2c65f6c87e0e4d62a2
Lars_vdv <larsvdvelden@live.nl>
2025-09-29 16:18:22 +0200

    modified:   Bioinformatics_in_Python/main2.py

--- Bioinformatics_in_Python/main2.py
+++ Bioinformatics_in_Python/main2.py
@@ -5,20 +5,19 @@ from pprint import pprint
 import math
 
                
-
 parse_fasta(path=r"C:\Users\larsv\OneDrive\Documenten\VSC\Bioinformatics_project\Bioinformatics_in_Python\test_data\rosalind_gc.txt", seq_type="DNA", create_variables=True)
 
 
-# print(seq_1.get_seq_info())
-# print(seq_1.countNucFrequency(color=True))
-# print(colored(seq_1.transcription()))
-# print(seq_1.reverse_complement())
-# print(seq_1.gc_content_round())
-# print(seq_1.gc_content())
-# print(seq_1.gc_content_subsec())
-# print(seq_1.translate_seq())
-# print(seq_1.codon_usage('L'))
-# print(seq_1.gen_reading_frames())
-# print(seq_1.all_proteins_from_ORF(ordered=True))
+print(seq_1.get_seq_info())
+print(seq_1.countNucFrequency(color=True))
+print(colored(seq_1.transcription()))
+print(seq_1.reverse_complement())
+print(seq_1.gc_content_round())
+print(seq_1.gc_content())
+print(seq_1.gc_content_subsec())
+print(seq_1.translate_seq())
+print(seq_1.codon_usage('L'))
+print(seq_1.gen_reading_frames())
+print(seq_1.all_proteins_from_ORF(ordered=True))
 
 
