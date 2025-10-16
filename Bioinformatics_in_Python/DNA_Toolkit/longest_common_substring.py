04c2d794953502f7a304f4978277008049baa23a
Lars_vdv <larsvdvelden@live.nl>
2025-10-16 16:12:58 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   Bioinformatics_in_Python/DNA_Toolkit/longest_common_substring.py
    	modified:   Bioinformatics_in_Python/DNA_Toolkit/test_data/rosalind_lcsm.txt

--- Bioinformatics_in_Python/DNA_Toolkit/longest_common_substring.py
+++ Bioinformatics_in_Python/DNA_Toolkit/longest_common_substring.py
@@ -20,16 +20,8 @@ print(getattr(seq[0], "seq_len")) #test if parsing was successful
 
 seq = sorted(seq, key=lambda s: s.seq_len, reverse=True)
 
-# #first order the sequences on length
-# seq = sorted(seq, key=lambda s: , reverse=True)
-# print(seq)
 
-for i in len(seq):
-    print(getattr(seq[i], "seq_len"))
-
-
-
-common_substrings = {}
+all_substrings = {}
 for i in range(len(seq)):
     for j in range(i+1, len(seq)):
         string1 = getattr(seq[i], "seq")
@@ -37,14 +29,23 @@ for i in range(len(seq)):
         match = SequenceMatcher(None, string1, string2).find_longest_match(0, len(string1), 0, len(string2)) 
         #find.longest_match is based on a[start1:end1] == b[start2:end2]
         matching_substring = string1[match.a: match.a + match.size] # Extract the matching substring
-        if matching_substring not in common_substrings:
-            common_substrings[matching_substring] = 1
+        if matching_substring not in all_substrings:
+            all_substrings[matching_substring] = 1
         else:
-            common_substrings[matching_substring] += 1
+            all_substrings[matching_substring] += 1
 
-print(common_substrings)
 
-max_len = max(len(k) for k in common_substrings) 
+common_substrings = []
+for i in range(len(seq)):
+    for j in range(len(all_substrings)):
+        if list(all_substrings.keys()[j]) in common_substrings in getattr(seq[i], "seq"):
+            common_substrings += all_substrings[j]
+        else:
+            continue
+    
+
+
+
 #max finds the longest length of the keys in the dictionary
 #len(k) for k in common_substrings iterates over all the keys of the dictionary. You dont need a forloop nor keys() method
 lcs = [k for k in common_substrings if len(k) == max_len] 
