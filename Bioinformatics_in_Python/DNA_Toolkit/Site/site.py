f4bd7e8372df69d28154f846e045647ec02c2cc6
Lars_vdv <larsvdvelden@live.nl>
2025-10-17 11:46:18 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Bioinformatics_in_Python/DNA_Toolkit/Site/site.py
    	modified:   Bioinformatics_in_Python/DNA_Toolkit/__pycache__/utilities.cpython-313.pyc
    	modified:   Bioinformatics_in_Python/DNA_Toolkit/utilities.py

--- Bioinformatics_in_Python/DNA_Toolkit/Site/site.py
+++ Bioinformatics_in_Python/DNA_Toolkit/Site/site.py
@@ -10,11 +10,24 @@ import bio_seq
 import bio_structs
 import utilities
 
-st.write(sys.executable)
-st.write(sys.path)
-
 st.header("Genome Toolit Plug-in Tool", divider = "violet")
 
 seq = bio_seq.bio_seq()
 seq.generate_rnd_seq(length = 100, seq_type = "DNA")
-st.write(seq)
+
+st.markdown("Randomly generated DNA string:")
+st.markdown(f":red[Sequence:] {getattr(seq, "seq")}")
+st.markdown(f":red[Sequence length:] {getattr(seq, "seq_len")}")
+st.markdown(f":red[Sequence type:] {getattr(seq, "seq_type")}")
+st.markdown(f":red[Sequence label:] {getattr(seq, "label")}")
+
+st.subheader("Sequence modification and analysis:")
+st.badge("Import your own FASTA.txt file below",icon ="🧬")
+
+uploaded_file = st.file_uploader("Choose a file", type="txt")
+type_of_seq = st.radio("What is the sequence type?", ["DNA","RNA","Protein"])
+
+st.write(type(uploaded_file))
+uploaded_file = utilities.parse_fasta_streamlit(uploaded_file, seq_type=type_of_seq)
+
+
--- Bioinformatics_in_Python/DNA_Toolkit/utilities.py
+++ Bioinformatics_in_Python/DNA_Toolkit/utilities.py
@@ -73,6 +73,7 @@ def parse_fasta(path, seq_type, create_variables=False):
     label = None
     seq_parts = [] #list of sequence parts until the next label is found
 
+
     with open(path, "r") as f:
         count = 0
         for line in f:
@@ -147,4 +148,66 @@ def protein_scrape_uniprot(path=""):
         temp_dict[f"{temp2[i]}"] = temp_dict[f"{temp2[i]}"].replace("\n","")  
         #cant use var_name here because it will always refer to the last value of i from the previous loop
         #line.replace because strip only removes from start and end of string, not middles of string
-    return temp_dict    
+    return temp_dict
+
+def parse_fasta_streamlit(file, seq_type, create_variables=False):
+    """
+    !! ASSIGN VARIABLE TO FUNCTION TO CREATE LIST !!
+    Read a FASTA file with one or more records and returns a list of bio_seq instances, one per record.
+    If create_variables is True, create global variables seq_1, seq_2, etc. for each sequence.
+    If a sequence is invalid for the given seq_type, it will be skipped with a warning.
+    """
+     # Avoid circular import
+    records = [] #collection of bio_seq objects
+    label = None
+    seq_parts = [] #list of sequence parts until the next label is found
+
+
+
+    count = 0
+    for line in file:
+        count += line.count('>')
+        
+    st.write(f"There are {count} total sequences in this file.")
+
+
+    for line in file: # Iterate through each line in the file
+        line = line.strip() # Remove leading/trailing whitespace
+        if not line: # Skip empty lines
+            continue
+
+        if line.startswith(">"): #when a new '>' is found, signals start of a new sequence
+            if label is not None: #f label is already set (meaning you just finished reading one record), you:
+                full_seq = "".join(seq_parts) #join all parts of the sequence into one string
+                temp_seq = bio_seq(full_seq, seq_type, label,raise_on_invalid=False) #create a new bio_seq object and add it to the records list
+                if temp_seq.is_valid == True:
+                    records.append(temp_seq) # add the bio_seq object to the records list only if it's valid
+                else:
+                    st.write(f"Warning: Skipping invalid {seq_type} sequence with label '{label}'")
+            label = line[1:] #if label is None, set label to the current line without '>'
+            seq_parts = [] #reset seq_parts for the next sequence
+        else:
+            seq_parts.append(line.upper()) # add the current line (part of the sequence) to seq_parts, converting to uppercase
+
+    if label is not None: #read the last record after the loop ends
+        full_seq = "".join(seq_parts)
+        temp_seq = bio_seq(full_seq, seq_type, label,raise_on_invalid=False)
+        if temp_seq.is_valid:
+            records.append(temp_seq)
+        else:
+            st.write(f"Warning: Sequence '{label}' is invalid for type '{seq_type}' and will be skipped.")
+    st.write(f"Successfully parsed {len(records)} valid {seq_type} sequences from the file.")
+    
+    if create_variables == True:
+        sequences_dict = {}
+         #The enumerate function wraps any iterable and yields pairs of (index, element), allowing you to track positions without manual counter management.
+        for i, record in enumerate(records): # Loop through each record with its index, index starts at 0 and ends at records length -1
+            var_name = f"seq_{i+1}" # Create a variable name like seq_1, seq_2, etc.
+            sequences_dict[var_name] = record 
+            st.write(f"Created variable '{var_name}' for sequence with label '{record.label}'")
+            st.write(id(sequences_dict))
+        return sequences_dict
+
+    else:
+        return records
+    
