6b6c90cfe25b8532ce2e84013f98e203c28fe280
Lars_vdv <larsvdvelden@live.nl>
2025-11-18 16:45:29 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Bioinformatics_in_Python/DNA_Toolkit/Stronghold/RNA_splicing.py
    	new file:   Bioinformatics_in_Python/DNA_Toolkit/Stronghold/splice.txt

--- Bioinformatics_in_Python/DNA_Toolkit/Stronghold/RNA_splicing.py
+++ Bioinformatics_in_Python/DNA_Toolkit/Stronghold/RNA_splicing.py
@@ -26,4 +26,57 @@ for i in range(len(gene_and_introns)):
 
 
 
- 
+def find_motif(dna_seq, motif):
+    """Find all occurrences of a motif in a DNA sequence."""
+    pattern = f'(?={motif})'  # Lookahead assertion to find overlapping motifs
+    position_start = [m.start() + 1 for m in re.finditer(pattern, dna_seq)]  # +1 to convert to 1-based index
+    return position_start
+
+for j in range(len(gene_and_introns)):
+    
+    #set whole gene sequence
+    if j == 0:
+        gene = gene_and_introns.get("seq_1")
+
+    #once first intron is removed, use new gene generated from removing the intron
+    #repeat this
+    else:
+        gene = gene_without_introns
+
+    #set name of key in dictionary so you can retrieve it
+    val = f"seq_{j+2}"
+
+    #get value of key
+    intron = gene_and_introns.get(val)
+
+    #last round no intron, break
+    if intron == None:
+        break
+
+    length_intron = len(intron)
+
+    #find the intron location in gene, start position is +1 indexing (See find motif function)
+    position_start = find_motif(gene, intron)
+    position_start = position_start[0]
+
+    #position_end is the start  position + the length of the intron
+    position_end = position_start + length_intron
+
+    #slice out the intron
+    gene_without_introns = gene[:position_start] + gene[position_end:]
+
+
+# print(gene_without_introns)
+# print(len(gene_without_introns))
+# print(gene_and_introns["seq_1"])
+# print(len(gene_and_introns["seq_1"]))
+
+#convert sequence to bio.seq object and translate it into protein sequence
+protein = bio_seq(seq=gene_without_introns)
+protein = bio_seq.translate_seq(protein)
+protein = "".join(protein)
+print(protein)
+
+
+
+write_file("splice.txt", protein)
